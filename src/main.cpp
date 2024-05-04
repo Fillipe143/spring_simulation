1337f0b4dafd7597ccab9afb0fce4e013a62f399
Fillipe143 <fillipedev.contato@gmail.com>
2024-05-04 14:14:39 -0300

    added methods for rendering particles and springs

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@ const int screen_height = 800;
 const int target_fps = 60;
 
 const float gravity_force = 1;
+const float default_k_value = 0.1;
+const float default_rest_length = 200;
+
+const float particle_radius = 20;
 
 void loop();
 
@@ -31,6 +35,14 @@ Spring new_spring(float k, float rest_length, Particle a, Particle b) {
     return Spring { k, rest_length, a, b };
 }
 
+void render_particle(Particle* self) {
+    DrawCircle(self->pos.x, self->pos.y, particle_radius, BLUE);
+}
+
+void render_spring(Spring* self) {
+    DrawLine(self->a.pos.x, self->a.pos.y, self->b.pos.x, self->b.pos.y, WHITE);
+}
+
 int main() {
     SetTraceLogLevel(LOG_WARNING);
     InitWindow(screen_width, screen_height, "SpringSimulation");
@@ -42,8 +54,13 @@ int main() {
     return 0;
 }
 
+Particle particles[] = { new_particle(screen_width / 2.0f, particle_radius), new_particle(screen_width / 2.0f, 200) };
+Spring springs[] = {new_spring(default_k_value, default_rest_length, particles[0], particles[1])};
+
 void loop() {
     BeginDrawing();
     ClearBackground(BLACK);
+    for (auto spring : springs) render_spring(&spring);
+    for (auto particle : particles) render_particle(&particle);
     EndDrawing();
 }
